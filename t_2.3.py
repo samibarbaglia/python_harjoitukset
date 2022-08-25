c006d1c58f44660ec0479503806f344d5ace6e9b
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-08-25 12:45:19 +0300

    tehtävä 2.3

---
+++ t_2.3.py
@@ -0,0 +1,10 @@
+kanta = float(input("Anna suorakulmion kanta   "))
+korkeus = float(input("Anna suorakulmion korkeus "))
+
+# laskutoimitukset
+ala = kanta * korkeus
+piiri = (2 * kanta) + (2 * korkeus)
+
+# tulostukset
+print("Suorakulmion pinta-ala = " + str(ala))
+print(f"Suorakulmion piiri = {piiri}")
