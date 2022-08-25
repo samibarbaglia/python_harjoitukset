f281753fd803e300234a66f7e2e99c59e8196c39
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-08-25 13:59:26 +0300

    tehtävä 2.4

--- t_2.4.py
+++ t_2.4.py
@@ -1,4 +1,5 @@
-# Kirjoita ohjelma, joka kysyy kolme kokonaislukua. Ohjelma tulostaa lukujen summan, tulon ja keskiarvon.
+# Kirjoita ohjelma, joka kysyy kolme kokonaislukua. Ohjelma tulostaa lukujen summan,
+# tulon ja keskiarvon.
 
 # Syötteet
 luku_1 = int(input("Anna 1. kokonaisluku "))
@@ -8,9 +9,14 @@ luku_3 = int(input("Anna 3. kokonaisluku "))
 # Laskutoimitukset
 summa = luku_1 + luku_2 + luku_3
 tulo = luku_1 * luku_2 * luku_3
-keskiarvo = summa / 3
+keskiarvo = (summa) / 3
 
 # tulostus
-print(summa)
-print(tulo)
-print(keskiarvo)
+# print(summa)
+# print(tulo)
+# print(keskiarvo)
+
+print("\nLukujen summa: " + str(summa) + "\nLukujen tulo: " + str(tulo) +
+      "\nLukujen keskiarvo: " + str(keskiarvo))
+
+print(f"\nLukujen summa: {summa} \nLukujen tulo: {tulo} \nLukujen keskiarvo: {keskiarvo}")
