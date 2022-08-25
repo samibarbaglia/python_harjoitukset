a2a9e7131bed399936e04f671f7d79f491d61fee
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-08-25 13:25:15 +0300

    tehtävä 2.1

--- t_2.1.py
+++ t_2.1.py
@@ -1,9 +1,2 @@
 nimi = input("Anna nimesi: ")
 input("Terve, " + nimi + "!")
-
-import math
-
-r = float(input("Ympyrän säde: "))
-ala = math.pi * r * r
-
-print("Ympytän pinta-ala on: " + str(ala))
