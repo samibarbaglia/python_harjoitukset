2ce1daf822d578859666c9627ae2d56870944039
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-09-05 19:10:34 +0300

    mod3

--- mod. 5/teht_5.1.py
+++ mod. 5/teht_5.1.py
@@ -1,7 +1,7 @@
 import random
 
 diceAmount = int(input("Anna arpakuutioiden määrä numeroina: "))
-dice = random.randint(1,6)
+dice = random.randint(1, 6)
 summa = 0
 
 for i in range(diceAmount):
--- mod. 5/teht_5.3.py
+++ mod. 5/teht_5.3.py
@@ -1,8 +1,10 @@
 userNumber = int(input("Testattava kokonaisnumero: "))
 numbers = [1, 2, 3, 5, 7]
 
-while userNumber % 1 or 2 or 3 or 5 or 7 == 0:
-    print("ei")
-    print("Uusiks")
-if userNumber % len(numbers) != 0:
-    print("joo.")
+for i in range(1):
+    if userNumber in range() == 0 and not numbers:
+        print("ei")
+    if userNumber % (1 or 2 or 3 or 5 or 7) != 0:
+        print("joo.")
+
+ÄÄÄÄÄÄÄÄÄÄ
