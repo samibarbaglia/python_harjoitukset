46c95f2daf44c9438625409b71e86b920baef70c
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-09-07 18:10:44 +0300

    mod

--- mod. 6/teht_6.5.py
+++ mod. 6/teht_6.5.py
@@ -11,4 +11,5 @@ def poista(lista):
     return lista
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lista)
 print(poista(lista))
--- mod. 6/teht_6.6.py
+++ mod. 6/teht_6.6.py
@@ -4,5 +4,26 @@
 # kahden pizzan halkaisijat ja hinnat sekä ilmoittaa, kumpi pizza antaa
 # paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
 # Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+import math
+def laskehinta(pizzaHinnat):
+        nm = 4
+        for i in pizzaHalkaisijat:
+            hinta = math.pi * (i/2) * (i/2)
+            for luku in pizzaHinnat:
+                luku = hinta / nm
+        return pizzaHinnat
 
-def
+pizzaHalkaisijat = []
+pizzaHinnat = []
+
+for i in range(2):
+    halkaisija = int(input(f"Anna {i + 1}. pizzan halkaisija (cm): "))
+    pizzaHalkaisijat.append(halkaisija)
+
+for i in range(2):
+    hinta = input(f"Anna {i+1}. pizzan hinta: ")
+    pizzaHinnat.append(hinta)
+
+print(laskehinta(pizzaHinnat))
+
+ÄÄÄÄÄÄÄ
