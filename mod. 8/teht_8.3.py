df36232326a67d3a20c0bd57fdc9a6964d7e7676
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-09-14 00:15:40 +0300

    mod

--- mod. 8/teht_8.3.py
+++ mod. 8/teht_8.3.py
@@ -4,3 +4,31 @@
 # avulla: https://geopy.readthedocs.io/en/stable/. Asenna kirjasto valitsemalla
 # View / Tool Windows / Python Packages. Kirjoita hakukenttään geopy ja vie asennus loppuun.
 
+#input("Anna 1. lentokentän ICAO-koodi: ")
+#input("Anna 2. lentokentän ICAO-koodi: ")
+
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port= 3306,
+    database='flight_game',
+    user='root',
+    password='p4r!i3',
+    autocommit=True
+    )
+
+def Haku():
+        UserIcao = input(f"Anna lentokentän ICAO-koodi: ")
+        sql = "SELECT name FROM airport WHERE ident ='" + UserIcao + "'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        tulos = kursori.fetchall()
+        return tulos
+
+ICAO = Haku()
+ICAO2 = Haku()
+
+print(f"{ICAO}, {ICAO2}")
+
+# En ymmärtänyt miten käyttää geopyä
